571ff1e13c71f400690ec8111852b0197e1fb4da
Flouis2017 <315587951@qq.com>
2019-02-25 01:20:01 +0800

    re learning

---
+++ re_learning/day20190224.py
@@ -0,0 +1,34 @@
+
+import re
+
+# 原生正则表达式：r"<reg_expression>"
+
+# 例1-2 用正则表达式判断是否包含数字字符
+# print(re.search(r"[0123456789]", "Hello 123 World!") is not None)
+
+# 例1-4 使用^和$进行全匹配
+# print(re.search(r"[0123456789]", "123") is not None)
+# print(re.search(r"^[0123456789]$", "123") is not None)
+
+# print(re.search(r"[0123456789]", "3") is not None)
+# print(re.search(r"^[0123456789]$", "3") is not None)
+
+# 简析：
+# ^：匹配开始，$：匹配结束，中间只有一个单字符匹配（即只有一对方括号[]），所以首先可以确定这个
+# 正则匹配出来的字符串长度是1，长度超过1或空字符串都是无法匹配的，接着看正则来确定具体匹配的内容。
+# 所以r"^[0123456789]$"对"3"可以匹配，但是对"123"就无法匹配了。
+
+# 范围表示符：-
+# 例1-5 [0-9]是合法的，[9-0]会报错
+# print(re.search(r"^[0-9]$", "2") is not None)
+# print(re.search(r"^[9-0]$", "2") is not None)
+# print(re.search(r"^[\x30-\x45]$", "2") is not None)
+
+
+
+
+
+
+
+
+
