b9c4a8020ef9037716b3866c8ae2d872403b4b4c
Flouis2017 <315587951@qq.com>
2019-03-09 21:34:41 +0800

    update in 20190309

--- re_learning/part_one.py
+++ re_learning/part_one.py
@@ -61,32 +61,79 @@ import re
 # print("Beijing\r\n2008 Olympic")
 
 # 例2-3 长度限定符：{m,n} {0,n} {n,} *({0,}) +({1,}) ?({0,1})
-print(re.search(r"^\d{4,6}$", "123") is not None)
-print(re.search(r"^\d{4,6}$", "1234") is not None)
-print(re.search(r"^\d{4,6}$", "123456") is not None)
-print(re.search(r"^\d{4,6}$", "1234567") is not None)
-print()
-print(re.search(r"^\d{4,}$", "123") is not None)
-print(re.search(r"^\d{4,}$", "1234") is not None)
-print(re.search(r"^\d{4,}$", "1234567") is not None)
-print()
-print(re.search(r"^\d{0,6}$", "") is not None)
-print(re.search(r"^\d{0,6}$", "123") is not None)
-print(re.search(r"^\d{0,6}$", "1234567") is not None)
-print()
+# print(re.search(r"^\d{4,6}$", "123") is not None)
+# print(re.search(r"^\d{4,6}$", "1234") is not None)
+# print(re.search(r"^\d{4,6}$", "123456") is not None)
+# print(re.search(r"^\d{4,6}$", "1234567") is not None)
+# print()
+# print(re.search(r"^\d{4,}$", "123") is not None)
+# print(re.search(r"^\d{4,}$", "1234") is not None)
+# print(re.search(r"^\d{4,}$", "1234567") is not None)
+# print()
+# print(re.search(r"^\d{0,6}$", "") is not None)
+# print(re.search(r"^\d{0,6}$", "123") is not None)
+# print(re.search(r"^\d{0,6}$", "1234567") is not None)
+# print()
 
 # 例2-4 长度限定符?的应用
-print(re.search(r"^colou?r$", "color") is not None, end=", This is American English\n")
-print(re.search(r"^colou?r$", "colour") is not None, end=", This is British English\n")
-print()
+# print(re.search(r"^colou?r$", "color") is not None, end=", This is American English\n")
+# print(re.search(r"^colou?r$", "colour") is not None, end=", This is British English\n")
+# print()
 
 # 例2-5 长度限定符+的应用
-print(re.search(r"^<[^>]+>$", "<div>") is not None)
-print(re.search(r"^<[^>]+>$", "</div>") is not None)
-print(re.search(r"^<[^>]+>$", "<>") is not None)
-print()
+# print(re.search(r"^<[^>]+>$", "<div>") is not None)
+# print(re.search(r"^<[^>]+>$", "</div>") is not None)
+# print(re.search(r"^<[^>]+>$", "<>") is not None)
+# print()
 
 # 例2-5 长度限定符*的应用
-print(re.search(r"^<[^>]*>$", "<div>") is not None)
-print(re.search(r"^<[^>]*>$", "</div>") is not None)
-print(re.search(r"^<[^>]*>$", "<>") is not None)
+# print(re.search(r"^<[^>]*>$", "<div>") is not None)
+# print(re.search(r"^<[^>]*>$", "</div>") is not None)
+# print(re.search(r"^<[^>]*>$", "<>") is not None)
+
+# 例2-7 通过re.search()返回的MatchObject提取数据
+# print(type(re.search(r"\d{3}", "ab123cd").group(0)))
+# print(re.search(r">[\w]+<", "<span>HelloWorld</span><em>test</em>").group(0))
+# print()
+
+# 例2-8 使用re.findall()提取数据
+# print(type(re.findall(r">[\w]+<", "<span>HelloWorld</span><em>test</em>")))
+# 可以遍历输出
+# for match_text in re.findall(r">[\w]+<", "<span>HelloWorld</span><em>test</em>"):
+#     match_text = match_text.replace("o", "a")
+#     print(match_text[1:-1])
+# 从上面可以看出re.search提取的数据是按就近原则匹配出的第一个，re.findall则是找出所有符合的数据
+
+# 例2-10 单行匹配符.
+# print(re.search(r"^[^\n]*$", "a12s3d4*(f&^%") is not None)
+# print(re.search(r"^.*$", "a12s3d4*(f&^%") is not None)
+# print(re.search(r"^.*$", "a12s3d4\n*(f&^%") is not None)
+
+# 例2-11 单行模式下.可以匹配任意字符
+# print(re.search(r"(?s)^.*$", "a12s3d4\n*(f&^%") is not None)
+
+# 测试 贪婪/懒惰匹配：
+# print(re.search(r"'.*'", "'quoted string' and another'").group(0))
+# print(re.search(r"'.*?'", "'quoted string' and another'").group(0))
+
+# html_source = '''
+# <script type="text/javascript">
+# alert("make a post request");
+# </script>
+#
+# <script>
+# alert("your request has been handled successfully");
+# </script>'''
+
+# js_regex = r"<script[\s>][\s\S]*?</script>"
+# for match_text in re.findall(js_regex, html_source):
+#     print(match_text + "\n")
+
+# 例2-18 使用正则表达式拆解Windows的路径
+# 注意：Windows下的路径分隔符为\，而\在正则表达式中是转移符，所以，使用两个\来表示\自身
+# windows_url = "D:\\CloudMusic\\Local\\Loop\\The xx - Intro.mp3"
+# print("file dir: " + re.search(r"^.*\\", windows_url).group(0))
+# print("file name: " + re.search(r"[^\\]*$", windows_url).group(0))
+
+
+
